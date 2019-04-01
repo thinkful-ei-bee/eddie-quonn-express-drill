73e83a9b4542d381e10c7683b30aef03dec438cb
quonn-bernard <qber83@gmail.com>
2019-04-01 09:18:53 -0700

    added endpoint "/cipher" and got cipher response

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+
 app.use(express.static('public'));
 app.get('/sum', (req, res) => {
 const c = parseFloat(req.query.a) + parseFloat(req.query.b);
@@ -9,6 +10,16 @@ const c = parseFloat(req.query.a) + parseFloat(req.query.b);
      res.send(`The sum of ${req.query.a} and ${req.query.b} is ${c}`);
   });
 
+  app.get('/cipher', (req, res) => {
+    const result = req.query.text + parseFloat(req.query.shift);
+
+         let textString = req.query.text.split('').map((char) => {
+            
+            return String.fromCharCode(char.charCodeAt(0) + parseFloat(req.query.shift));
+         });
+
+         res.send(textString.join(''));
+      });
   
   app.listen(8080, () => {
       console.log("express is running!");
