92deb58366eb8607d34dd043da335dd8fc7d8428
quonn-bernard <qber83@gmail.com>
2019-04-01 09:01:10 -0700

    added a server and got a response

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+app.get('/sum', (req, res) => {
+const c = parseFloat(req.query.a) + parseFloat(req.query.b);
+     ;
+     res.send(`The sum of ${req.query.a} and ${req.query.b} is ${c}`);
+  });
+
+  
+  app.listen(8080, () => {
+      console.log("express is running!");
+  })
